dfe76ba75dd0adc8645ff4dd220bd599d4faef78
unknown <opkd0710@gmail.com>
2020-05-27 16:24:24 +0900

    StationSelector廃止

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -2,18 +2,16 @@
     <div id="footer">
         <div class="container">
             <DateSelector v-on:selected="sendDate"></DateSelector>
-<!--            <StationSelector v-on:selected="sendStation"></StationSelector> -->
         </div>
     </div>
 </template>
 
 <script>
 import DateSelector from './selector/DateSelector'
-import StationSelector from './selector/StationSelector'
 
 export default {
     components: {
-        DateSelector, StationSelector
+        DateSelector
     },
     methods: {
         sendDate: function(payload){
